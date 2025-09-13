e086e28b36239aa59d17079b311952c7b85d9d03
YehyunLee <glad7cu@gmail.com>
2025-09-13 19:26:32 -0400

    ios support

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* LaunchAR SDK: loads early to enable WebXR on iOS via polyfill/redirect */}
+        <script src={`https://launchar.app/sdk/v1?key=${process.env.NEXT_PUBLIC_LAUNCHAR_SDK_KEY}&redirect=true`}></script>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
