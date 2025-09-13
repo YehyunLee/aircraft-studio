a10f91d9b42b232d3b5afba6f23e2f2c6d26fe0f
YehyunLee <glad7cu@gmail.com>
2025-09-13 02:13:51 -0400

    auth0 work in progress. still error

--- src/pages/api/auth/[...auth0].js
+++ src/pages/api/auth/[...auth0].js
@@ -1,3 +1,7 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { auth0 } from "../../../lib/auth0";
 
-export default handleAuth();
+export default async function handler(req, res) {
+	// The `auth0` client exposes `handleAuth` as a method that accepts the req/res
+	// and will route to login/callback/logout/etc. This matches the v4 SDK usage.
+	return auth0.handleAuth(req, res);
+}
