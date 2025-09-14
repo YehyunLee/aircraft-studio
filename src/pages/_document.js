ffea3dfe77488b035186ae64bb523b3a6843e2d5
YehyunLee <glad7cu@gmail.com>
2025-09-14 02:34:29 -0400

    favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,9 @@ export default function Document() {
       <Head>
         {/* LaunchAR SDK: load as early as possible in the document head */}
         <script src={`https://launchar.app/sdk/v1?key=${sdkKey}&redirect=true`}></script>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
       <body className="antialiased">
         <Main />
