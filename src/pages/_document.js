f188dadcd713ff3c670935f12dd886282b916435
YehyunLee <glad7cu@gmail.com>
2025-09-13 22:29:00 -0400

    key env

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const sdkKey = process.env.NEXT_PUBLIC_LAUNCHAR_SDK_KEY;
   return (
     <Html lang="en">
       <Head>
         {/* LaunchAR SDK: load as early as possible in the document head */}
-        <script src="https://launchar.app/sdk/v1?key=cQ5j8qCUsFSnAeFXLjdFlWiy7pZrvEpL&redirect=true"></script>
+        <script src={`https://launchar.app/sdk/v1?key=${sdkKey}&redirect=true`}></script>
       </Head>
       <body className="antialiased">
         <Main />
